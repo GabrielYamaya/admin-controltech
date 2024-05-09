8be954e456f2ae10f3c2b0d78bb7b914b8c95be4
FelipeCherry3 <felipeelias609@hotmail.com>
2024-05-09 15:34:34 -0300

    login Implement

--- admin-controltech/src/app/login/login/login.component.ts
+++ admin-controltech/src/app/login/login/login.component.ts
@@ -4,6 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { LoginComponentSuccessfulDialogComponent } from '../../components/login-component-successful-dialog/login-component-successful-dialog.component';
 import { UsersService } from '../../services/users.service';
 import { LoginComponentFailDialogComponent } from '../../components/login-component-fail-dialog/login-component-fail-dialog.component';
+import { error } from 'node:console';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -27,51 +28,54 @@ export class LoginComponent {
     private userService: UsersService
   ) {}
 
-  loginButton() {
+  async loginButton() {
+    try {
+        await this.fetchUser();
+        if (this.formDataUser.login === this.usuarioAuth?.login && this.formDataUser.user_password === this.usuarioAuth.user_password) {
+            console.log('Usuário autenticado com sucesso!');
+            this.openSuccessDialog();
+            setTimeout(() => {
+                window.location.href = '/ativos';
+            }, 2000);
+        } 
+    } catch (error) {
+        this.openFailDialog();
+              setTimeout(() => {
+                  location.reload();
+              }, 2000);
+        console.error('Erro ao realizar login:', error);
+        // Tratar o erro conforme necessário
+    }
+}
 
-    this.fetchUser();
-    // Simulando lógica de autenticação
-    if (this.formDataUser.login === this.usuarioAuth?.login && this.formDataUser.user_password === this.usuarioAuth.user_password) {
-        console.log('Usuário autenticado com sucesso!');
-        this.loginSucess =  true;
-        if (this.loginSucess) {
-          this.openSuccessDialog();
-          
-          setTimeout(() => {
-            // Redirecionamento aqui
-            window.location.href = '/ativos';
-          }, 2000); // 2000 milissegundos = 2 segundos
-        } else {
-          this.loginSucess = false;
-          this.openFailDialog();
-          setTimeout(() => {
-            // Redirecionamento aqui
-            location.reload;
-          }, 2000);
-        }
-      }
-    }
-
-    redirectToCadastro() {
-        // Redirecionar para a página de cadastro
-        window.location.href = '/login/cadastrar';
-    }
-    openSuccessDialog() {
-      this.dialog.open(LoginComponentSuccessfulDialogComponent, {
-        width: '250px',
-        data: {} // Dados opcionais para passar para o diálogo
-      });
-    }
-    openFailDialog() {
-      this.dialog.open(LoginComponentFailDialogComponent, {
-        width: '250px',
-        data: {} // Dados opcionais para passar para o diálogo
-      });
-    }
-    fetchUser() {
-      this.userService.getUserByUsername(this.formDataUser.login).subscribe(usuarioAuth => {
-        this.usuarioAuth = usuarioAuth;
-        console.log(this.usuarioAuth)
-      });
-    }
+fetchUser(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        this.userService.getUserByUsername(this.formDataUser.login).subscribe(
+            (usuarioAuth) => {
+                this.usuarioAuth = usuarioAuth;
+                console.log(this.usuarioAuth);
+                resolve();
+            },
+            (error) => {
+                reject(error);
+            }
+        );
+    });
+}
+  redirectToCadastro() {
+      // Redirecionar para a página de cadastro
+      window.location.href = '/login/cadastrar';
+  }
+  openSuccessDialog() {
+    this.dialog.open(LoginComponentSuccessfulDialogComponent, {
+      width: '250px',
+      data: {} // Dados opcionais para passar para o diálogo
+    });
+  }
+  openFailDialog() {
+    this.dialog.open(LoginComponentFailDialogComponent, {
+      width: '250px',
+      data: {}
+    });
+  }
 }
